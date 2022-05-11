f54fc82daee738356022f43cf72f9f24d6275b3b
zhangfan <cyouho@hotmail.co.jp>
2022-05-11 09:44:07 +0800

    设置home页面显示

---
+++ resources/views/home/home_contents.blade.php
@@ -0,0 +1,20 @@
+<main class="container">
+    <div class="row m-auto">
+        <div class="col">
+            <div class="jumbotron rounded p-4 p-md-5 custom-jumbotron bg-blue text-blue">
+                <p>学的不仅是技术，更是梦想！！！</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron rounded p-4 p-md-5 custom-jumbotron bg-green text-green">
+                <h4>家用货架管理系统</h4>
+                <a href="#">点击进入</a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron rounded p-4 p-md-5 custom-jumbotron">
+                <p>学的不仅是技术，更是梦想！！！</p>
+            </div>
+        </div>
+    </div>
+</main>
