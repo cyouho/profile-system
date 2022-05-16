093dfab2cc8208460b616f2e7d12ef2569122982
zhangfan <cyouho@hotmail.co.jp>
2022-05-16 10:22:48 +0800

    添加profile页面显示用户基本信息功能

--- app/Http/Controllers/Utils.php
+++ app/Http/Controllers/Utils.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Exception\ClientException;
 
 class Utils extends Controller
 {
-    public static function guzzleGet(string $url, array $postData)
+    public static function guzzleGet(string $url)
     {
         try {
             $client = new Client();
@@ -16,7 +16,6 @@ class Utils extends Controller
                 'header' => [
                     'Accept' => 'application/json',
                 ],
-                'form_params' => $postData,
                 'http_errors' => FALSE, // api 会返回自己的错误信息，这些不计入 http 错误
             ]);
             $statusCode = $response->getStatusCode();
@@ -31,5 +30,9 @@ class Utils extends Controller
             'http_status' => $statusCode,
             'response_contents' => $response,
         ];
+    }
+
+    public function guzzlePost()
+    {
     }
 }
